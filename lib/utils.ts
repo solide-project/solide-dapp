561d99c64414ce8f71b512741dd989ab6cafe424
unknown <z5208980@ad.unsw.edu.au>
2023-12-23 20:08:23 +1100

    update for prod

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const SiteMessage = "As Solide is hosting on Hobby tier, be mindful of potential compilation timeouts. If you encounter delays, wait a few minutes for optimal service loading. Our ongoing development efforts aim to improve performance. Thank you for your patience!"
-export const SolideURL = "http://localhost:3001"
-// export const SolideURL = "https://solide.vercel.app"
+// export const SolideURL = "http://localhost:3001"
+export const SolideURL = "https://solide.vercel.app"
