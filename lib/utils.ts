fcda4bedf68062b0747bc8555bb31c6a0ee39440
unknown <z5208980@ad.unsw.edu.au>
2023-12-23 14:15:28 +1100

    change extension for prod

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const SolideURL = "http://localhost:3001"
-// export const SolideURL = "https://solide.vercel.app"
+// export const SolideURL = "http://localhost:3001"
+export const SolideURL = "https://solide.vercel.app"
