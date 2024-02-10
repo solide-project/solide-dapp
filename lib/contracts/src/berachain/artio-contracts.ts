71f9b9b85b4d69745d3ed5f16f454b4e621c03c2
5208980 <z5208980@zmail.unsw.edu.au>
2024-02-10 11:02:32 +1100

    update contracts

--- lib/contracts/src/berachain/artio-contracts.ts
+++ lib/contracts/src/berachain/artio-contracts.ts
@@ -4,15 +4,14 @@ import { getNetworkNameFromChainID } from "@/lib/chains/name"
 const chain = ChainID.BERACHAIN_TESTNET
 export const items = [
     {
-        title: "Deployed Contracts: Honey Token",
-        tutorial: "",
-        reference: "https://docs.berachain.com/developers/contracts/honey",
-        description: `Source code for the HONEY token in ${getNetworkNameFromChainID(ChainID.BERACHAIN_TESTNET)}`,
-        playground: {
-            default: {
-                address: "https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC20.sol",
-                chainId: chain,
-            },
-        },
-    },
+        "title": "Berachain: Honey Token",
+        "tutorial": "https://github.com/solide-project/solide-guides/blob/master/src/berachain/Honey/README.md",
+        "reference": "https://docs.berachain.com/developers/contracts/honey",
+        "description": `Source code for the HONEY token in ${getNetworkNameFromChainID(ChainID.BERACHAIN_TESTNET)}`,
+        "playground": {
+            "default": {
+                "address": "https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC20.sol"
+            }
+        }
+    }
 ]
